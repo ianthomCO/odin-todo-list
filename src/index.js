252a8e4221624b9493c873d8fc238c8354ea6cb4
ianthomCO <ian.b.thom@gmail.com>
2023-03-16 21:30:27 -0600

    Test of nav bar slide funcationality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './style/style.scss'
+
+const sidebarBtn = document.querySelector('.sidebar');
+
+
+let mobileMenuOpen = false;
+
+sidebarBtn.addEventListener('click',()=>{
+    mobileMenuOpen = !mobileMenuOpen;
+    if(mobileMenuOpen){
+        document.querySelector('nav').style.left = '0';
+    } else {
+        document.querySelector('nav').style.left = '-18rem';
+    }
+})
